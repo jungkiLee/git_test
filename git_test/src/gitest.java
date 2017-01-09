2b7be24dd409151e3b311c7c4de9f38362e1335f
JK Lee <dlwjdrl1112@naver.com>
2017-01-09 14:45:16 +0900

    sheddang

---
+++ git_test/src/gitest.java
@@ -0,0 +1,9 @@
+/**
+ * Created by ljk on 2017-01-09.
+ */
+public class gitest {
+    public void test() {
+        System.out.print("i have a pen");
+        System.out.print("i have an apple");
+    }
+}
